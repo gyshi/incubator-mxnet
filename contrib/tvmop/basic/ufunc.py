1256d3e7165ee3fe145213ffc1df04c7c29b8a0c
sguangyo <1360024032@qq.com>
2019-08-20 10:28:46 +0800

    add tvm op exp2

--- contrib/tvmop/basic/ufunc.py
+++ contrib/tvmop/basic/ufunc.py
@@ -17,6 +17,7 @@
 
 # coding: utf-8
 import tvm
+import topi
 from .. import defop, AllTypes
 
 def compute_add(dtype, ndim):
@@ -48,3 +49,63 @@ def vadd_gpu(dtype, ndim):
     s[C].bind(bx, tvm.thread_axis("blockIdx.x"))
     s[C].bind(tx, tvm.thread_axis("threadIdx.x"))
     return s, [A, B, C]
+
+def compute_exp2(dtype, ndim):
+    A = tvm.placeholder([tvm.var() for _ in range(ndim)], name='A', dtype=dtype)
+    B = tvm.compute([tvm.var() for _ in range(ndim)],
+                    lambda *index: topi.power(2, A[index]), name='B')
+    s = tvm.create_schedule(B.op)
+    return s, A, B
+
+@defop(name="exp2", target="cpu", auto_broadcast=True,
+       dtype=["float32", "float64"], ndim=list(range(1, 6)))
+def exp2(dtype, ndim):
+    s, A, B = compute_exp2(dtype, ndim)
+    axes = [axis for axis in B.op.axis]
+    fused = s[B].fuse(*axes)
+    s[B].parallel(fused)
+
+    return s, [A, B]
+
+@defop(name="cuda_exp2", target="cuda", auto_broadcast=True,
+       dtype=["float32", "float64"], ndim=list(range(1, 6)))
+def exp2_gpu(dtype, ndim):
+    s, A, B= compute_exp2(dtype, ndim)
+    s = tvm.create_schedule(B.op)
+    axes = [axis for axis in B.op.axis]
+    fused = s[B].fuse(*axes)
+    bx, tx = s[B].split(fused, factor=64)
+    s[B].bind(bx, tvm.thread_axis("blockIdx.x"))
+    s[B].bind(tx, tvm.thread_axis("threadIdx.x"))
+    return s, [A, B]
+
+def compute_backward_exp2(dtype, ndim):
+    A = tvm.placeholder([tvm.var() for _ in range(ndim)], name='A', dtype=dtype)
+    B = tvm.placeholder([tvm.var() for _ in range(ndim)], name='B', dtype=dtype)
+    C = tvm.placeholder([tvm.var() for _ in range(ndim)], name='C', dtype=dtype)
+    D = tvm.compute([tvm.var() for _ in range(ndim)],
+                    lambda *index:  A[index] * B[index] * C[index] / 2, name='D')
+    s = tvm.create_schedule(D.op)
+    return s, A, B, C, D
+
+@defop(name="backward_exp2", target="cpu", auto_broadcast=True,
+       dtype=["float32", "float64"], ndim=list(range(1, 6)))
+def backward_exp2(dtype, ndim):
+    s, A, B, C, D = compute_exp2(dtype, ndim)
+    axes = [axis for axis in D.op.axis]
+    fused = s[D].fuse(*axes)
+    s[D].parallel(fused)
+
+    return s, [A, B, C, D]
+
+@defop(name="cuda_backward_exp2", target="cuda", auto_broadcast=True,
+       dtype=["float32", "float64"], ndim=list(range(1, 6)))
+def backward_exp2_gpu(dtype, ndim):
+    s, A, B, C, D= compute_exp2(dtype, ndim)
+    s = tvm.create_schedule(D.op)
+    axes = [axis for axis in D.op.axis]
+    fused = s[D].fuse(*axes)
+    bx, tx = s[D].split(fused, factor=64)
+    s[D].bind(bx, tvm.thread_axis("blockIdx.x"))
+    s[D].bind(tx, tvm.thread_axis("threadIdx.x"))
+    return s, [A, B, C, D]
--- src/operator/contrib/tvmop/ufunc.cc
+++ src/operator/contrib/tvmop/ufunc.cc
@@ -61,6 +61,72 @@ NNVM_REGISTER_OP(_contrib_tvm_vadd)
     .set_attr<mxnet::FCompute>("FCompute<gpu>", mxnet::op::TVMBroadcastCompute<func_vadd_gpu>);
 #endif  // MXNET_USE_CUDA
 
+static constexpr char func_exp2_cpu[] = "exp2";
+static constexpr char func_epx2_gpu[] = "cuda_exp2";
+static constexpr char func_backward_epx2_cpu[] = "backward_exp2";
+static constexpr char func_backward_epx2_gpu[] = "cuda_backward_exp2";
+
+template<const char* func>
+void TVMOpExp2Compute(const nnvm::NodeAttrs& attrs,
+                      const mxnet::OpContext& ctx,
+                      const std::vector<TBlob>& inputs,
+                      const std::vector<OpReqType>& req,
+                      const std::vector<TBlob>& outputs) {
+  CHECK_EQ(inputs.size(), 1U);
+  CHECK_EQ(outputs.size(), 1U);
+  tvm::runtime::TVMOpModule::Get()->Call(func, ctx, {inputs[0], outputs[0]});
+}
+
+//template<int req>
+//struct exp2_backward {
+//  template<typename DType>
+//  MSHADOW_XINLINE static void Map(int i, DType* in_grad, const DType* in_data1,
+//                                  const DType* in_data2, const DType* out_grad) {
+//    DType grad = in_data2[i] * in_data1[i] / DType(2);
+//    KERNEL_ASSIGN(in_grad[i], req, grad);
+//  }
+//};
+
+template<const char* func>
+void TVMExp2Backward(const nnvm::NodeAttrs& attrs,
+                     const OpContext& ctx,
+                     const std::vector<TBlob>& inputs,
+                     const std::vector<OpReqType>& req,
+                     const std::vector<TBlob>& outputs) {
+  CHECK_EQ(inputs.size(), 3U);
+  CHECK_EQ(outputs.size(), 1U);
+  CHECK_EQ(req.size(), 1U);
+  using namespace mshadow;
+  const TBlob& out_grad = inputs[0];
+  const TBlob& in_data1 = inputs[1];
+  const TBlob& in_data2 = inputs[2];
+  const TBlob& in_grad = outputs[0];
+  tvm::runtime::TVMOpModule::Get()->Call(func, ctx, {out_grad, in_data1, in_data2, in_grad});
+}
+
+NNVM_REGISTER_OP(_contrib_tvm_exp2)
+    .set_num_inputs(1)
+    .set_num_outputs(1)
+    .add_argument("data", "NDArray-or-Symbol", "resources")
+    .set_attr<mxnet::FInferShape>("FInferShape", mxnet::op::ElemwiseShape<1, 1>)
+    .set_attr<nnvm::FInferType>("FInferType", mxnet::op::ElemwiseType<1, 1>)
+#if MXNET_USE_CUDA
+    .set_attr<mxnet::FCompute>("FCompute<gpu>", mxnet::op::TVMOpExp2Compute<func_epx2_gpu>)
+#endif  // MXNET_USE_CUDA
+    .set_attr<mxnet::FCompute>("FCompute<cpu>", mxnet::op::TVMOpExp2Compute<func_exp2_cpu>)
+    .set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseInOut{ "_backward_exp2" });
+
+
+
+NNVM_REGISTER_OP(_backward_exp2)
+.set_num_inputs(3)
+.set_num_outputs(1)
+.set_attr<nnvm::TIsBackward>("TIsBackward", true)
+#if MXNET_USE_CUDA
+.set_attr<FCompute>("FCompute<gpu>", mxnet::op::TVMExp2Backward<func_backward_epx2_gpu>)
+#endif  // MXNET_USE_CUDA
+.set_attr<FCompute>("FCompute<cpu>", mxnet::op::TVMExp2Backward<func_backward_epx2_cpu>);
+
 }  // namespace op
 }  // namespace mxnet
 #endif  // MXNET_USE_TVM_OP
